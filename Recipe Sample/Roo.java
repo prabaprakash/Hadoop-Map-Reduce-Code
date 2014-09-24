468d73477195ebf6b8a4b6c76bc3bf5b4997ce17
prabaprakash <prabakaran1993@hotmail.com>
2014-09-25 00:08:41 +0530

    Recipe

---
+++ Recipe Sample/Roo.java
@@ -0,0 +1,39 @@
+ class Id
+{
+
+    public String oid;
+}
+
+
+ class Ts
+{
+
+    public long date ;
+}
+
+public class Roo
+{
+
+    public Id _id ;
+
+    public String name ;
+
+    public String ingredients ;
+
+    public String url ;
+
+    public String image ;
+
+    public Ts ts ;
+
+    public String cookTime;
+
+    public String source ;
+
+    public String recipeYield ;
+    public String datePublished;
+
+    public String prepTime ;
+
+    public String description;
+}
